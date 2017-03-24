832de716f6d6bf9d9f61bff68b17bb4a5abe776d
Hannah Davis <worldofmagic48@gmail.com>
2017-03-24 15:44:27 -0500

    wrote exchange framework

--- our_stuff/polynomial.py
+++ our_stuff/polynomial.py
@@ -14,10 +14,13 @@ class Polynomial:
         else:
             while len(self.__coeffs)<degree:
                 self.__coeffs.append(random.randint(-sizelimit,sizelimit)%mod)
-                
+
         for index  in range(len(self.__coeffs)):
             self.__coeffs[index] %=mod
-
+        self.mod=mod
+        self.degree=degree
+    def __repr__(self):
+        print(self.__coeffs)
     def coeff_to_byte(x):
         firstbyte=x//256
         secondbyte=x%256
@@ -28,24 +31,57 @@ class Polynomial:
         for coeff in self.__coeffs:
             sequence+=Polynomial.coeff_to_byte(coeff)
         return bytes(sequence)
-        
+    def __add__(self,other):
+        c=[(self.__coeffs[i]+other.__coeffs[i])%self.mod for i in range(self.degree)]
+        return Polynomial(coeffs=c,mod=self.mod,degree=self.degree)
+    def __mul__(self,other):
+        #FIXME implement this please
+        return Polynomial()
     def signal(self):
-        info_bits=''
-        random=random.randint(0,1)
+        info_bits=[]
         for coeff in self.__coeffs:
-            if abs(coeff)-1>q//4:
-                info_bits+='1'
+            if abs(coeff)-1>self.mod//4:
+                info_bits+=[1]
             else:
-                info_bits+='0'
-        info_list=b''
-        while len(info_bits)>3:
-            byte=info_bits[0:3]
-            info_bits=info_bits[3:]
-            info_list+=bytes([int(byte,2)])
-        return info_list
-        
-    def mod2(self):
-        return [ x%2 for x in self.__coeffs]
-        
+                info_bits+=[0]
+        return Polynomial(coeffs=info_bits,degree=self.degree,mod=self.mod)
+
+    def mod2(self,sig):
+        return [ (self.__coeffs[i]+sig.__coeffs[i]*sig.mod/2)%self.mod %2 for i in range(self.degree)]
+
     def reconcile(v,w):
         v+=(w*(q-1)/2).mod2()
+
+class Authority():
+    def __init__(self, clientA=None, clientB=None):
+        self.a=Polynomial()
+        if clientA is None:
+            self.clientA=KeyExchanger()
+        else:
+            self.clientA=clientA
+        if clientB is None:
+            self.clientB=KeyExchanger()
+        else:
+            self.clientB=clientB
+    def runExchange(self):
+        pA=self.clientA.sendP(self.a)
+        sig, pB = self.clientB.key_and_signal(self.a,pA)
+        self.clientA.key_and_signal(self.a,pB)
+    def checkExchange(self):
+        print(self.clientA.key)
+        print(self.clientB.key)
+
+class KeyExchanger():
+    def __init__(self):
+        self.secret=Polynomial(sizelimit=12889//4)
+        self.error=Polynomial(sizelimit=12889//4)
+        self.key=None
+    def sendP(self,a):
+         return self.secret*a+self.error
+
+    def key_and_signal(self,a,p,signal=None):
+        poly=self.secret*p
+        if signal is None:
+            signal=poly.signal()
+        self.key=poly.mod2(signal)
+        return poly.signal(), self.secret*a+self.error
