a50d37559fcad8fe8ecaceafa075afeb2c8801f3
Tyler Westland <tylerofthewest@gmail.com>
2017-03-24 08:13:13 -0500

    Consolidated inits

--- our_stuff/polynomial.py
+++ our_stuff/polynomial.py
@@ -1,27 +1,34 @@
 import random
 class Polynomial:
-    degree=1024
-    mod=12289
-    def __init__(self, sizelimit=mod//2):
-        self.__coeffs=[random.randint(-b,b)%Polynomial.mod for x in range(Polynomial.degree)]
-    def __init__(self, coeffs=None):
+    def __init__(self, coeffs=None, sizelimit=None, degree=1024, mod=12289):
         if coeffs is None:
             coeffs=[]
         self.__coeffs=coeffs
-        while len(self.__coeffs)<Polynomial.degree:
-            self.__coeffs.append(0)
-        i=0
-        while i<len(self.__coeffs):
-            self.__coeffs[i] %=Polynomial.mod
+
+        if sizelimit is None:
+            sizelimit = mod//2
+
+        if sizelimit == 0:
+            while len(self.__coeffs)<Polynomial.degree:
+                self.__coeffs.append(0)
+        else:
+            while len(self.__coeffs)<Polynomial.degree:
+                self.__coeffs.append(random.randint(-sizelimit,sizelimit)%Polynomial.mod)
+                
+        for index  in range(len(self.__coeffs)):
+            self.__coeffs[index] %=Polynomial.mod
+
     def coeff_to_byte(x):
         firstbyte=x//256
         secondbyte=x%256
         return firstbyte,secondbyte
+
     def to_bits(self):
         sequence=()
         for coeff in self.__coeffs:
             sequence+=Polynomial.coeff_to_byte(coeff)
         return bytes(sequence)
+        
     def signal(self):
         info_bits=''
         random=random.randint(0,1)
@@ -36,7 +43,9 @@ class Polynomial:
             info_bits=info_bits[3:]
             info_list+=bytes([int(byte,2)])
         return info_list
+        
     def mod2(self):
         return [ x%2 for x in self.__coeffs]
+        
     def reconcile(v,w):
         v+=(w*(q-1)/2).mod2()
