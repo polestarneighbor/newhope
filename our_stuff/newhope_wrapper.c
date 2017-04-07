1354c60d71a2f51193f016266fb6b986416e1da6
Hannah Davis <worldofmagic48@gmail.com>
2017-04-07 13:02:12 -0500

    started wrapper code

---
+++ our_stuff/newhope_wrapper.c
@@ -0,0 +1,26 @@
+#include "../../avx2/newhope.h"
+#include "../../avx2/poly.h"
+#include "../../avx2/randombytes.h"
+#include "../../avx2/crypto_stream_chacha20.h"
+#include "../../avx2/error_correction.h"
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  poly sk_a, pk_a,  pk_adv;
+  //pk_adv=convert whatever poly python sends as arg
+  unsigned char key_a[32], key_b[32];
+  unsigned char senda[NEWHOPE_SENDABYTES];
+  unsigned char sendb[NEWHOPE_SENDBBYTES];
+  unsigned char seed[NEWHOPE_SEEDBYTES];
+  newhope_keygen(senda, &sk_a);
+  decode_a(&pk_a,seed, senda)
+  encode_a(senda, &pk_adv, seed)
+  //Bob derives a secret key and creates a response
+  newhope_sharedb(key_b, sendb, senda);
+  poly bp, c;
+  decode_b(&bp, &c, sendb)
+  // write *bp and *c to a file somehow
+  return 0;
+}
